fa34f6f643200fe2808ce28f0da5a975fdfac39a
aniteka <an1teka4@gmail.com>
2024-03-07 03:36:23 -0800

    feat: create and init project, first simple design

---
+++ payments-system-uni-lab/LoginForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace payments_system_uni_lab
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginSwitherButton = new System.Windows.Forms.Button();
+            this.loginUsernameTextBox = new System.Windows.Forms.TextBox();
+            this.loginPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginSwitherButton
+            // 
+            this.loginSwitherButton.Location = new System.Drawing.Point(15, 18);
+            this.loginSwitherButton.Name = "loginSwitherButton";
+            this.loginSwitherButton.Size = new System.Drawing.Size(107, 33);
+            this.loginSwitherButton.TabIndex = 0;
+            this.loginSwitherButton.Text = "User";
+            this.loginSwitherButton.UseVisualStyleBackColor = true;
+            // 
+            // loginUsernameTextBox
+            // 
+            this.loginUsernameTextBox.Location = new System.Drawing.Point(304, 173);
+            this.loginUsernameTextBox.Name = "loginUsernameTextBox";
+            this.loginUsernameTextBox.Size = new System.Drawing.Size(144, 22);
+            this.loginUsernameTextBox.TabIndex = 1;
+            // 
+            // loginPasswordTextBox
+            // 
+            this.loginPasswordTextBox.Location = new System.Drawing.Point(304, 201);
+            this.loginPasswordTextBox.Name = "loginPasswordTextBox";
+            this.loginPasswordTextBox.PasswordChar = '*';
+            this.loginPasswordTextBox.Size = new System.Drawing.Size(144, 22);
+            this.loginPasswordTextBox.TabIndex = 2;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(327, 253);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(98, 35);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.loginPasswordTextBox);
+            this.Controls.Add(this.loginUsernameTextBox);
+            this.Controls.Add(this.loginSwitherButton);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button loginButton;
+
+        private System.Windows.Forms.TextBox loginUsernameTextBox;
+        private System.Windows.Forms.Button loginSwitherButton;
+        private System.Windows.Forms.TextBox loginPasswordTextBox;
+
+        private System.Windows.Forms.TabPage userLoginPage;
+        private System.Windows.Forms.TabPage adminLoginPage;
+
+        #endregion
+    }
+}
