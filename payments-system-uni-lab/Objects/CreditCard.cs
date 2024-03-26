70051d26099a5534bfdc52139366cb3f44c69dac
aniteka <an1teka4@gmail.com>
2024-03-26 03:05:43 -0700

    create classes

---
+++ payments-system-uni-lab/Objects/CreditCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using payments_system_uni_lab.Persons;
+
+namespace payments_system_uni_lab.Objects
+{
+    class CreditCard
+    {
+        public UInt64 Num { get; private set; }
+        public UInt16 Cvc { get; private set; }
+        public UInt64 ClientMoney { get; private set; }
+        public UInt64 CreditLimit { get; private set; }
+        public Client Client { get; private set; }
+
+        public CreditCard(Client cardClient)
+        {
+            // take from db
+            Num = 1234123412341234;
+            Cvc = 123;
+            ClientMoney = 333;
+            CreditLimit = 444;
+            Client = cardClient;
+        }
+    }
+}
---
+++ payments-system-uni-lab/Persons/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using payments_system_uni_lab.Objects;
+
+namespace payments_system_uni_lab.Persons
+{
+    class Client
+    {
+        public CreditCard CreditCard { get; private set; }
+
+        public Client()
+        {
+            CreditCard = new CreditCard(this);
+        }
+    }
+}
